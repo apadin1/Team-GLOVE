09d667b5ca74bde8b8bdeeed4aaae4c771d8c578
Tim <twschum@gmail.com>
2016-10-04 23:22:22 -0400

    Added essentially empty headers

---
+++ touch_sensor.h
@@ -0,0 +1,21 @@
+/*
+ * Filename:  touch_sensor.h
+ * Project:   EECS 473 - Team GLOVE
+ * Date:      Fall 2016
+ * Authors:
+ *     Nick Bertoldi
+ *     Ben Heckathorn
+ *     Ryan O’Keefe
+ *     Adrian Padin
+ *     Tim Schumacher
+ *
+ * Purpose:
+ *
+ */
+
+#ifndef TOUCH_SENSOR_H_
+#define TOUCH_SENSOR_H_
+
+
+#endif /* TOUCH_SENSOR_H_ */
+
