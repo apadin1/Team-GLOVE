1a5deb578df1a27a0e2883fb53f77959186080f5
Adrian Padin <apadin1@users.noreply.github.com>
2016-10-04 21:56:47 -0400

    Create imu.h

---
+++ imu.h
@@ -0,0 +1,14 @@
+/* Filename:  imu.h
+ * Project:   EECS 473 - Team GLOVE
+ * Date:      4 October 2016
+ * 
+ * Interface for reading data from IMU
+ */
+ 
+#ifndef IMU_H
+#define IMU_H
+
+
+#endif /* IMU_H */
+
+
