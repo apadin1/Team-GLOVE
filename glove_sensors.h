b3e459186ccef58f35aa15699c9b465b682d033f
Tim <tim@deepfield>
2016-10-06 00:23:27 -0400

    Fleshed out glove_sensors

--- glove_sensors.h
+++ glove_sensors.h
@@ -21,17 +21,31 @@
 #include <FreeRTOS.h>
 #include <semphr.h>
 
+#include "flex_sensor.h"
+#include "touch_sensor.h"
+#include "imu.h"
+
+#define GLOVE_N_FLEX_SENSORS 6
+
+
 /*
- *  This is the condenced sensor data struct used on either
+ * This is the condenced sensor data struct used on either
  * end of the bluetooth connection, must remain in sync on both
  * ends of the connection
  */
 typedef struct {
 
-
-    xSemaphoreHandle data_mutex;
-
 } glove_sensors_raw_t;
+
+/*
+ * This version is the container for all the sensors on the glove,
+ * using their top-level interfaces.
+ */
+typedef struct {
+    flex_sensor_t* flex_sensors[GLOVE_N_FLEX_SENSORS];
+    touch_sensor_t* touch_sensor;
+    bno_imu_t* imu;
+} glove_sensors_interfaced_t;
 
 
 #endif /* GLOVE_SENSORS_H_ */
