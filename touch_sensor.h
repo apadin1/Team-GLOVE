aa2e3c0888189f3c17a83382f41299860460d4a2
Tim <tim@deepfield>
2016-10-05 20:33:03 -0400

    Working on touch_sensor

--- touch_sensor.h
+++ touch_sensor.h
@@ -10,12 +10,32 @@
  *     Tim Schumacher
  *
  * Purpose:
+ *   Top-level interface for working with capacitive sensor chip,
+ *   which uses I2C and can communicate the values of up to 8 sensors
  *
  */
 
 #ifndef TOUCH_SENSOR_H_
 #define TOUCH_SENSOR_H_
 
+#include <inttypes.h>
+
+#define TOUCH_SENSOR_NUM 4
+
+/* touch_sensor_t
+ *
+ * deflection: scaled analog-to-digital value read from the GPIO
+ */
+typedef struct {
+    uint8_t id;
+    uint16_t capacitance;
+} touch_sensor_t;
+
+/*
+ * Given an array of touch_sensor_t, update each
+ * of those structs.  Assumes TOUCH_SENSOR_NUM sensors
+ */
+void update_touch_sensors(touch_sensor_t *sensor);
 
 #endif /* TOUCH_SENSOR_H_ */
 
