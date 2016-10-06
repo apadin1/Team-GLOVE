3241c754bab7175ce14405d18d6f83b90302b3c0
Adrian Padin <apadin1@users.noreply.github.com>
2016-10-05 22:07:47 -0400

    Update heartrate_sensor.h

--- heartrate_sensor.h
+++ heartrate_sensor.h
@@ -17,18 +17,37 @@
 #define HEARTRATE_SENSOR_H_
 
 
+#include <inttypes.h>
+
 #include <FreeRTOS.h>
 
-#define SMOOTHING_WINDOW 5;
+#define SMOOTHING_WINDOW 5; /* Number of previous samples to save */
 
+/* heartrate_sensor_t
+ *
+ * analog_pin: GPIO pin number, as determined by the GPIO lib
+ * deflection: scaled analog-to-digital value read from the GPIO
+ */
 typedef struct {
     TickType_t last_pulse_ticks[SMOOTHING_WINDOW];
     uint8_t analog_pin;
-} heart_rate_sensor_t;
+} heartrate_sensor_t;
 
-void initHeartRate(heart_rate_sensor_t *, uint8_t analog_pin);
-uint16_t readHeartRateValue(heart_rate_sensor_t *);
+/*
+ * initialize the gpio pins for the heartrate sensor
+ */
+heartrate_sensor_t * initHeartRate(uint8_t analog_pin);
+
+/*
+ * read the current signal value on the heart rate monitor
+ */
+uint16_t readHeartRateSignalValue(heart_rate_sensor_t *);
+
+/*
+ * return the heart rate based on the period of pulses
+ */
 float getHeartRate(heart_rate_sensor_t *);
+
 
 #endif /* HEARTRATE_SENSOR_H_ */
 
