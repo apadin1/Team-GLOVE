b2123ca6198c79ff1cb8cbf8e808963ad0969622
Tim <tim@deepfield>
2016-10-05 20:11:34 -0400

    Finished flex_sensor

--- flex_sensor.h
+++ flex_sensor.h
@@ -10,18 +10,25 @@
  *     Tim Schumacher
  *
  * Purpose:
+ *  Top-level interface to the flex sensors
  *
  */
 
 #ifndef FLEX_SENSOR_H_
 #define FLEX_SENSOR_H_
 
-struct flex_sensor {
-    int analog_pin;
-    int deflection;
-};
+#include <inttypes.h>
 
-int get_deflection();
+/* flex_sensor_t
+ *
+ * analog_pin: GPIO pin number, as determined by the GPIO lib
+ * deflection: scaled analog-to-digital value read from the GPIO
+ */
+typedef struct {
+    uint8_t analog_pin;
+    uint16_t deflection;
+} flex_sensor_t;
+
+int get_deflection(flex_sensor_t *sensor);
 
 #endif /* FLEX_SENSOR_H_ */
-
