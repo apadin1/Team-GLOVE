b7c336f0ff814832c171e33d4ca18201c7d901e3
Tim <twschum@gmail.com>
2016-10-05 21:38:50 -0400

    Added orientation/acceleration & update() for IMU

--- imu.h
+++ imu.h
@@ -19,16 +19,38 @@
 
 #include "Adafruit_BNO055.h" // will need to port
 
+typedef struct {
+
+    uint32_t orient_x;
+    uint32_t orient_y;
+    uint32_t orient_z;
+
+    uint32_t accel_x;
+    uint32_t accel_y;
+    uint32_t accel_z;
+
+} bno_imu_t;
+
 
 /*
- * We will want some way to save the calibration for the IMU
+ * Need some way to save the calibration for the IMU
  * and then reload it on setup
  */
 void saveIMUCalibration();
 void loadIMUCalibration();
 
+/*
+ * Initialization requires setting some defaults for using
+ * the sensor, handle all that here
+ *
+ * Return a pointer to the allocated imu
+ */
+bno_imu_t* initIMU();
 
-
+/*
+ * Update the orientation and acceleration information
+ */
+void updateIMU(bno_imu_t*);
 
 #endif /* IMU_H_ */
 
