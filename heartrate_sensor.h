83e68dd7d0a3a58d8c82f75d07081ae629bda864
Adrian Padin <apadin1@users.noreply.github.com>
2016-10-05 20:55:20 -0400

    Update heartrate_sensor.h

--- heartrate_sensor.h
+++ heartrate_sensor.h
@@ -17,5 +17,18 @@
 #define HEARTRATE_SENSOR_H_
 
 
+#include <FreeRTOS.h>
+
+#define SMOOTHING_WINDOW 5;
+
+typedef struct {
+    TickType_t last_pulse_ticks[SMOOTHING_WINDOW];
+    uint8_t analog_pin;
+} heart_rate_sensor_t;
+
+void initHeartRate(heart_rate_sensor_t *, uint8_t analog_pin);
+uint16_t readHeartRateValue(heart_rate_sensor_t *);
+float getHeartRate(heart_rate_sensor_t *);
+
 #endif /* HEARTRATE_SENSOR_H_ */
 
