5f61d135725f5294830e97882b5050d3021c1a4e
Tim <twschum@gmail.com>
2016-10-13 11:54:26 -0400

    Added the python install

--- mbed-glove-firmware/setup.sh
+++ mbed-glove-firmware/setup.sh
@@ -2,6 +2,13 @@
 # quick and dirty mbed setup
 set -x
 
+sudo pip install mbed-cli
+
 mbed deploy
 mkdir mbed-os/.git/info/
 mbed deploy
+
+sudo pip install -r mbed-os/requirements.txt
+
+set +x
+echo "\nYou're gonna need gcc-arm-none-eabi version 4.9"
