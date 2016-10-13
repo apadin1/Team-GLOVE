c6e132d0c78a326f30fc3cc0a2db1a1536a8e711
Tim <twschum@gmail.com>
2016-10-13 13:09:55 -0400

    Flash can do screen!

--- mbed-glove-firmware/fla.sh
+++ mbed-glove-firmware/fla.sh
@@ -7,12 +7,13 @@ show_help() { echo "$pre A shell script for Team GLOVE by Tim Schumacher";
 			echo "Compliments mbed-cli for 'flashing' the NRF51 dev board with compiled mbed hex files";
 			echo " By default, mounts the board (can attempt to find it automatically),";
 			echo "and copies over the compiled hex file.";
-			echo " Usage:\n      fla.sh [-h] [-v] [-n] [-c] [-u] [-d <dev_file>]";
+			echo " Usage:\n      fla.sh [-h] [-v] [-n] [-c] [-s] [-u] [-d <dev_file>]";
 			echo "      -h Show this help and exit";
 			echo "      -v Verbose output for debugging this script";
 			echo "      -n Dry-run, won't copy the hex file to the board";
 			echo "      -c Compile, will also run the default compile command";
-			echo "      -u Unmount, will unmount the board when finished";
+			echo "      -s Screen, drop into a screen on the serial debug port";
+			echo "      -u Do Not Unmount, will not unmount the board when finished";
 			echo "      -d <sda|sdb|sdc|...> Device file that is the board's JLINK USB filesystem (no /dev/ needed)";
 }
 
@@ -23,14 +24,16 @@ target="NRF51_DK"
 toolchain="GCC_ARM"
 prog_dir=${PWD##*/}
 program="$prog_dir".hex
+serial_dev=/dev/ttyACM0
 
 OPTIND=1 # A POSIX variable
 opt_v= # unset is falsy for [ $opt_v ]
 opt_n=
 opt_c=
+opt_s=
 opt_u=
 dev_file=
-while getopts "h?vncud:" opt; do
+while getopts "h?vncsud:" opt; do
 	case "$opt" in
 		h|\?)
 			show_help
@@ -41,6 +44,8 @@ while getopts "h?vncud:" opt; do
 		n)  opt_n=0
 			;;
 		c)  opt_c=0
+			;;
+		s)  opt_s=0
 			;;
 		u)  opt_u=0
 			;;
@@ -107,11 +112,21 @@ if [ ! $opt_n ]; then
 fi
 
 # unmount the board (if requested)
-if [ $opt_u ]; then
+if [ ! $opt_u ]; then
     if mount | grep $mountdir > /dev/null; then
         sync
         run sudo umount $mountdir
         echo "$pre Un-mounted $mountdir"
 	fi
+fi
+
+if [ $opt_s ]; then
+	echo "$pre Dropping into serial debug session in screen\n         Use Ctrl-A then :quit to exit\n"
+	sleep 1
+    echo "$pre Connecting....."
+	until [ -c $serial_dev ]; do
+		sleep 1
+	done
+	screen $serial_dev
 fi
 
--- mbed-glove-firmware/main.cpp
+++ mbed-glove-firmware/main.cpp
@@ -1,13 +1,39 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+Serial debug(USBTX, USBRX);
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
+AnalogIn flex(A0);
+float brightness = 0.0;
+
 int main() {
+    debug.printf("Press 'u' to turn LED1 brightness up, 'd' to turn it down\n");
+
+    /*
+    // (note the calls to Thread::wait below for delays)
     while (true) {
         led1 = !led1;
-        Thread::wait(500);
+        debug.printf("This is a thing\r\n");
+        Thread::wait(1000);
+    }
+    */
+
+    while(1) {
+        char c = debug.getc();
+        debug.putc(c);
+
+        if((c == 'u') && (brightness < 0.5)) {
+            brightness += 0.01;
+            led = brightness;
+        }
+        if((c == 'd') && (brightness > 0.0)) {
+            brightness -= 0.01;
+            led = brightness;
+        }
+
+        if ((c == 'r') {
+                debug.printf("\nFlex reading is: %f\n", flex);
+        }
+
     }
 }
-
