d31c42ee78d31053395cb5d4bf851dd86e86e66a
Tim <tim@deepfield>
2016-10-13 13:18:37 -0400

    Break test loops into functions

--- mbed-glove-firmware/main.cpp
+++ mbed-glove-firmware/main.cpp
@@ -1,39 +1,53 @@
 #include "mbed.h"
 
-DigitalOut led1(LED1);
+DigitalOut led(LED1);
 Serial debug(USBTX, USBRX);
 
 AnalogIn flex(A0);
 float brightness = 0.0;
 
-int main() {
-    debug.printf("Press 'u' to turn LED1 brightness up, 'd' to turn it down\n");
 
-    /*
+void blink() {
     // (note the calls to Thread::wait below for delays)
     while (true) {
-        led1 = !led1;
+        led = !led;
         debug.printf("This is a thing\r\n");
         Thread::wait(1000);
     }
-    */
+}
 
-    while(1) {
+void echo_term() {
+    debug.printf("Press 'u' to turn LED1 brightness up, 'd' to turn it down\n");
+    for (;;) {
         char c = debug.getc();
         debug.putc(c);
 
-        if((c == 'u') && (brightness < 0.5)) {
+        if ((c == 'u') && (brightness < 0.5)) {
             brightness += 0.01;
             led = brightness;
         }
-        if((c == 'd') && (brightness > 0.0)) {
+        if ((c == 'd') && (brightness > 0.0)) {
             brightness -= 0.01;
             led = brightness;
         }
 
-        if ((c == 'r') {
-                debug.printf("\nFlex reading is: %f\n", flex);
-        }
-
     }
+}
+
+void flex_read() {
+
+    for (;;) {
+        debug.printf("Flex reading is: %f\n", flex);
+        Thread::wait(1000);
+        /*
+        if (c == 'r') {
+            debug.printf("\nFlex reading is: %f\n", flex);
+        }
+        */
+    }
+}
+
+int main() {
+
+    flex_read();
 }
