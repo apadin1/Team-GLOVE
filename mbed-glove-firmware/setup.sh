29ca58b27306986037d1631d13ede65feb4047db
Tim <tim@deepfield>
2016-10-14 11:15:46 -0400

    More info in setup

--- mbed-glove-firmware/setup.sh
+++ mbed-glove-firmware/setup.sh
@@ -12,3 +12,5 @@ sudo pip install -r mbed-os/requirements.txt
 
 set +x
 echo "\n Don't forget to install GCC ARM embedded 4.9 https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update"
+echo "If you install it to /usr/local/ then use the following command to setup mbed"
+echo "mbed config -G /usr/local/gcc-arm-none-eabi-4_9-2015q3/bin && mbed config --list"
