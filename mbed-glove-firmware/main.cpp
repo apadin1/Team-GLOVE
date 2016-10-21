1fc84653255969756a59529195ee33bea20bfa10
Tim <tim@deepfield>
2016-10-21 11:09:14 -0400

    First interfacing is working on the IMU

--- mbed-glove-firmware/main.cpp
+++ mbed-glove-firmware/main.cpp
@@ -21,7 +21,6 @@ void blink() {
 }
 
 void imu_test() {
-    DigitalOut pwr_onoff(p13);
     I2C i2c(I2C_SDA0, I2C_SCL0); // p30, p7
     BNO055 imu(i2c, p12);  // Reset =D7, addr = BNO055_G_CHIP_ADDR, mode = MODE_NDOF <- as default
     led = 1;
@@ -29,31 +28,18 @@ void imu_test() {
 
     BNO055_ID_INF_TypeDef       bno055_id_inf;
     BNO055_EULER_TypeDef        euler_angles;
-    BNO055_QUATERNION_TypeDef   quaternion;
     BNO055_LIN_ACC_TypeDef      linear_acc;
-    BNO055_GRAVITY_TypeDef      gravity;
-    BNO055_TEMPERATURE_TypeDef  chip_temp;
 
     pc.printf("Starting IMU test\r\n");
-    /*
 
     imu.set_mounting_position(MT_P6);
-    pwr_onoff = 0;
-    pc.printf("\r\n\r\nIf pc terminal soft is ready, please hit any key!\r\n");
-    char c = pc.getc();
-    pc.printf("Bosch Sensortec BNO055 test program on " __DATE__ "/" __TIME__ "\r\n");
-    // Is BNO055 avairable?
 
     if (imu.chip_ready() == 0){
         do {
             pc.printf("Bosch BNO055 is NOT avirable!!\r\n Reset\r\n");
-            pwr_onoff = 1;  // Power off
-            wait(0.1);
-            pwr_onoff = 0;  // Power on
-            wait(0.02);
         } while(imu.reset());
     }
-    pc.printf("Bosch BNO055 is available now!!\r\n");
+
     pc.printf("AXIS_REMAP_CONFIG:0x%02x, AXIS_REMAP_SIGN:0x%02x\r\n",
             imu.read_reg0(BNO055_AXIS_MAP_CONFIG), imu.read_reg0(BNO055_AXIS_MAP_SIGN));
     imu.read_id_inf(&bno055_id_inf);
@@ -61,32 +47,21 @@ void imu_test() {
             bno055_id_inf.chip_id, bno055_id_inf.acc_id, bno055_id_inf.mag_id, bno055_id_inf.gyr_id);
     pc.printf("SW REV:0x%04x, BL REV:0x%02x\r\n",
             bno055_id_inf.sw_rev_id, bno055_id_inf.bootldr_rev_id);
-    pc.printf("If you would like to calibrate the BNO055, please hit 'y' (No: any other key)\r\n");
-    c = pc.getc();
-    if (c == 'y'){
-        // calibrate
-    }
-    */
     pc.printf("[E]:Euler Angles[deg],[Q]:Quaternion[],[L]:Linear accel[m/s*s],");
     pc.printf("[G]:Gravity vector[m/s*s],[T]:Chip temperature,Acc,Gyr[degC],[S]:Status,[M]:time[mS]\r\n");
     for (;;) {
         imu.get_Euler_Angles(&euler_angles);
-        pc.printf("[E],Y,%+6.1f,R,%+6.1f,P,%+6.1f,",
+        pc.printf("[E] %+6.1f Yaw, %+6.1f Pitch, %+6.1f Roll, ",
                 euler_angles.h, euler_angles.r, euler_angles.p);
-        imu.get_quaternion(&quaternion);
-        pc.printf("[Q],W,%d,X,%d,Y,%d,Z,%d,",
-                quaternion.w, quaternion.x, quaternion.y, quaternion.z);
+
         imu.get_linear_accel(&linear_acc);
-        pc.printf("[L],X,%+6.1f,Y,%+6.1f,Z,%+6.1f,",
+        pc.printf("[L] X: %+6.1f, Y: %+6.1f, Z: %+6.1f,\r\n",
                 linear_acc.x, linear_acc.y, linear_acc.z);
-        imu.get_gravity(&gravity);
-        pc.printf("[G],X,%+6.1f,Y,%+6.1f,Z,%+6.1f,",
-                gravity.x, gravity.y, gravity.z);
-        imu.get_chip_temperature(&chip_temp);
-        pc.printf("[T],%+d,%+d,",
-                chip_temp.acc_chip, chip_temp.gyr_chip);
+
+        /*
         pc.printf("[S],0x%x,[M],%d\r\n",
                 imu.read_calib_status(), t.read_ms());
+                */
 
         led = !led;
         Thread::wait(1000);
