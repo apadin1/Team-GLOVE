b9a34c8e3ab90270f0e8df4c4c112eaecd118225
Nick Bertoldi <bertoldinick@gmail.com>
2016-10-21 11:54:16 -0400

    Removed extraneous eagle files

---
+++ Team-GLOVE-master/template.h
@@ -0,0 +1,21 @@
+/*
+ * Filename:  XnameX.h
+ * Project:   EECS 473 - Team GLOVE
+ * Date:      Fall 2016
+ * Authors:
+ *     Nick Bertoldi
+ *     Ben Heckathorn
+ *     Ryan O’Keefe
+ *     Adrian Padin
+ *     Tim Schumacher
+ *
+ * Purpose:
+ *
+ */
+
+#ifndef TMP_H_
+#define TMP_H_
+
+
+#endif /* TMP_H_ */
+
