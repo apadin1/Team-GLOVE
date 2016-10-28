5945e0ef9f574790f55df9d6295a27994edfa91d
Tim <twschum@gmail.com>
2016-10-28 11:36:00 -0400

    Updated glove sensors definitions

--- mbed-glove-firmware/drivers/glove_sensors.h
+++ mbed-glove-firmware/drivers/glove_sensors.h
@@ -18,35 +18,19 @@
 #ifndef GLOVE_SENSORS_H_
 #define GLOVE_SENSORS_H_
 
-#include <FreeRTOS.h>
-#include <semphr.h>
-
 #include "flex_sensor.h"
 #include "touch_sensor.h"
 #include "imu.h"
 
-#define GLOVE_N_FLEX_SENSORS 6
-
-
 /*
- * This is the condenced sensor data struct used on either
- * end of the bluetooth connection, must remain in sync on both
- * ends of the connection
+ * Condenced sensor data struct used on either
+ * end of the bluetooth connection
  */
 typedef struct {
-
+    flex_sensor_t flex_sensors[FLEX_SENSORS_COUNT];
+    key_states_t touch_sensor;
+    bno_imu_t imu;
 } glove_sensors_raw_t;
-
-/*
- * This version is the container for all the sensors on the glove,
- * using their top-level interfaces.
- */
-typedef struct {
-    flex_sensor_t* flex_sensors[GLOVE_N_FLEX_SENSORS];
-    touch_sensor_t* touch_sensor;
-    bno_imu_t* imu;
-} glove_sensors_interfaced_t;
-
 
 #endif /* GLOVE_SENSORS_H_ */
 
