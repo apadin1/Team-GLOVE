d32f4b905c53082f06b6f8232ed3a65ec4779092
Tim <tim@deepfield>
2016-10-28 15:46:21 -0400

    Ran clang-format

--- mbed-glove-firmware/drivers/collector.h
+++ mbed-glove-firmware/drivers/collector.h
@@ -14,71 +14,71 @@
  *
  */
 
- #ifndef COLLECTOR_H_
- #define COLLECTOR_H_
+#ifndef COLLECTOR_H_
+#define COLLECTOR_H_
 
- #include <inttypes.h>
+#include <inttypes.h>
 
- #include "mbed.h"
- #include "glove_sensors.h"
+#include "mbed.h"
+#include "glove_sensors.h"
 
- /*
-  * Update Period (in seconds)
-  *
-  * 0.01 s = 10 ms = 100 Hz
-  */
- const float COLLECTOR_UPDATE_PERIOD = 0.01;
+/*
+ * Update Period (in seconds)
+ *
+ * 0.01 s = 10 ms = 100 Hz
+ */
+const float COLLECTOR_UPDATE_PERIOD = 0.01;
 
- /* Collector
-  *
-  * Single class to handle data collection and transmission
-  * between the gloves and the receiver. Methods are written
-  * to update internally, and to write all methods into a
-  * data structure
-  */
- class Collector {
- public:
-   /*
-    * Constructor for collector
-    */
-   Collector(FlexSensors* flex, IMU_BNO055* imu, TouchSensor* touch, Serial* pc=NULL);
+/* Collector
+ *
+ * Single class to handle data collection and transmission
+ * between the gloves and the receiver. Methods are written
+ * to update internally, and to write all methods into a
+ * data structure
+ */
+class Collector {
+public:
+    /*
+     * Constructor for collector
+     */
+    Collector(FlexSensors* flex, IMU_BNO055* imu, TouchSensor* touch,
+              Serial* pc = NULL);
 
-   /*
-    * Collect data from sensors
-    */
-   void get_data();
+    /*
+     * Collect data from sensors
+     */
+    void get_data();
 
-   /*
-    * Transmit data from the glove
-    */
-   void transmitData();
+    /*
+     * Transmit data from the glove
+     */
+    void transmitData();
 
-   /*
-    * Update the data contained in the collector
-    */
-   void update();
+    /*
+     * Update the data contained in the collector
+     */
+    void update();
 
-   /*
-    * Calls the start() method on the periodic update task,
-    * an internal timer is set up in the constructor
-    */
-   void startUpdateTask(float period_s=COLLECTOR_UPDATE_PERIOD);
+    /*
+     * Calls the start() method on the periodic update task,
+     * an internal timer is set up in the constructor
+     */
+    void startUpdateTask(float period_s = COLLECTOR_UPDATE_PERIOD);
 
-   /*
-    * Calls the stop() method on the periodic update timer,
-    */
-   void stopUpdateTask();
+    /*
+     * Calls the stop() method on the periodic update timer,
+     */
+    void stopUpdateTask();
 
- private:
-   FlexSensors* flex;
-   IMU_BNO055* imu;
-   TouchSensor* touch;
+private:
+    FlexSensors* flex;
+    IMU_BNO055* imu;
+    TouchSensor* touch;
 
-   glove_sensors_raw_t glove_data;
-   Mutex glove_mutex;
-   RtosTimer* update_task_timer;
+    glove_sensors_raw_t glove_data;
+    Mutex glove_mutex;
+    RtosTimer* update_task_timer;
 
-   Serial* pc; // for debugging
- };
- #endif /* COLLECTOR_H_ */
-
+    Serial* pc;  // for debugging
+};
+#endif /* COLLECTOR_H_ */
