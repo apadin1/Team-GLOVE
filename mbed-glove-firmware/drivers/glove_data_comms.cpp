0b809c1f3c5094f7c011764f73374b8d13303a2c
Tim <tim@deepfield>
2016-11-01 11:37:18 -0400

    Added a crc implementation

---
+++ mbed-glove-firmware/drivers/glove_data_comms.cpp
@@ -0,0 +1,32 @@
+/*
+ * Filename:  glove_data_comms.h
+ * Project:   EECS 473 - Team GLOVE
+ * Date:      Fall 2016
+ * Authors:
+ *     Nick Bertoldi
+ *     Ben Heckathorn
+ *     Ryan O’Keefe
+ *     Adrian Padin
+ *     Tim Schumacher
+ *
+ * Purpose:
+ *  Top-level interface for send & recv the glove sensor data
+ *
+ *  Will sit on top the BLE UART libraries
+ *  Implements CRC & header for the serialized struct
+ *
+ */
+
+#include "glove_data_comms.h"
+
+
+
+int GloveDataComms::sendSerialized(glove_data_raw_t* data) {
+
+}
+
+int GloveDataComms::recvSerialized(glove_data_raw_t* data) {
+
+}
+
+
