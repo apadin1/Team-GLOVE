0664cc7ba2ed94f31ccecc5c73312f6b4dd58f82
Tim <tim@deepfield>
2016-11-01 11:08:17 -0400

    Main header for communication of the glove data

---
+++ mbed-glove-firmware/drivers/glove_data_comms.h
@@ -0,0 +1,50 @@
+/*
+ * Filename:  glove_data_comms.h
+ * Project:   EECS 473 - Team GLOVE
+ * Date:      Fall 2016
+ * Authors:
+ *     Nick Bertoldi
+ *     Ben Heckathorn
+ *     Ryan O’Keefe
+ *     Adrian Padin
+ *     Tim Schumacher
+ *
+ * Purpose:
+ *  Top-level interface for send & recv the glove sensor data
+ *
+ *  Will sit on top the BLE UART libraries
+ *  Implements CRC & header for the serialized struct
+ *
+ */
+
+#ifndef GLOVE_DATA_COMMS_H_
+#define GLOVE_DATA_COMMS_H_
+
+#include <inttypes.h>
+
+class GloveDataComms {
+public:
+
+    /*
+     * Serializes a struct requires its length
+     *
+     * Return -1 on error
+     */
+    int sendSerialized(glove_data_raw_t* data);
+
+    /*
+     * Un-serializes a struct
+     *
+     * Return -1 on Error
+     *
+     * Maybe even have a checksum for funzies
+     */
+    int recvSerialized(glove_data_raw_t* data);
+
+private:
+    // BLE object
+
+};
+
+#endif /* GLOVE_DATA_COMMS_H_ */
+
