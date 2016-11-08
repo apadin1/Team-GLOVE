8178ea8bc62ac1b5ae405c7e70c0900179df706c
Tim <twschum@gmail.com>
2016-11-07 21:36:31 -0500

    First analog button class

---
+++ mbed-glove-firmware/drivers/analog_button.h
@@ -0,0 +1,22 @@
+/*
+ * Filename:  analog_button.h
+ * Project:   EECS 473 - Team GLOVE
+ * Date:      Fall 2016
+ * Authors:
+ *     Nick Bertoldi
+ *     Ben Heckathorn
+ *     Ryan O’Keefe
+ *     Adrian Padin
+ *     Tim Schumacher
+ *
+ * Purpose:
+ *
+ *  Simple wrapper around an analog sensor value,
+ *  to translate to a binary value on threshold.
+ *
+ */
+
+class AnalogButton {
+
+    AnalogButton();
+}
