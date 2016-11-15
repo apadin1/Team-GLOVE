63301aee2635d969e69172e77a24dfd51ef99759
Tim <twschum@gmail.com>
2016-11-15 14:09:16 -0500

    Formatting on translator

--- mbed-glove-firmware/drivers/translator.cpp
+++ mbed-glove-firmware/drivers/translator.cpp
@@ -18,283 +18,285 @@
 const PinName TRANSLATOR_DEBUG_PIN = p26;
 
 Translator::Translator(FlexSensors* _flex, IMU_BNO055* _imu,
-        TouchSensor* _touch, KeyboardMouse* _input)
-     : flex(_flex), imu(_imu), touch(_touch), HIDinput(_input), working(TRANSLATOR_DEBUG_PIN) {
+                       TouchSensor* _touch, KeyboardMouse* _input)
+    : flex(_flex), imu(_imu), touch(_touch), HIDinput(_input),
+      working(TRANSLATOR_DEBUG_PIN) {
 
-  /* FLEX1 */
-  AnalogButton<flex_sensor_t> flex1(&glove_data.flex_sensors[0], 300, 800, 0.2, false, false, false);
-  flex_sensors[FLEX1] = &flex1;
+    /* FLEX1 */
+    AnalogButton<flex_sensor_t> flex1(&glove_data.flex_sensors[0], 300, 800,
+                                      0.2, false, false, false);
+    flex_sensors[FLEX1] = &flex1;
 
-  /* FLEX2 */
-  AnalogButton<flex_sensor_t> flex2(&glove_data.flex_sensors[1], 300, 800, 20, false, false);
-  flex_sensors[FLEX2] = &flex2;
+    /* FLEX2 */
+    AnalogButton<flex_sensor_t> flex2(&glove_data.flex_sensors[1], 300, 800, 20,
+                                      false, false);
+    flex_sensors[FLEX2] = &flex2;
 
-  /* FLEX3 */
-  AnalogButton<flex_sensor_t> flex3(&glove_data.flex_sensors[2], 300, 800, 20, false, false);
-  flex_sensors[FLEX3] = &flex3;
+    /* FLEX3 */
+    AnalogButton<flex_sensor_t> flex3(&glove_data.flex_sensors[2], 300, 800, 20,
+                                      false, false);
+    flex_sensors[FLEX3] = &flex3;
 
-  /* FLEX4 */
-  AnalogButton<flex_sensor_t> flex4(&glove_data.flex_sensors[3], 300, 800, 20, false, false);
-  flex_sensors[FLEX4] = &flex4;
+    /* FLEX4 */
+    AnalogButton<flex_sensor_t> flex4(&glove_data.flex_sensors[3], 300, 800, 20,
+                                      false, false);
+    flex_sensors[FLEX4] = &flex4;
 
-  /* TOUCH1 */
-  AnalogButton<uint8_t> touch1(&glove_data.touch_sensor.a);
-  touch_sensors[TOUCH1] = &touch1;
+    /* TOUCH1 */
+    AnalogButton<uint8_t> touch1(&glove_data.touch_sensor.a);
+    touch_sensors[TOUCH1] = &touch1;
 
-  /* TOUCH2 */
-  AnalogButton<uint8_t> touch2(&glove_data.touch_sensor.b);
-  touch_sensors[TOUCH2] = &touch2;
+    /* TOUCH2 */
+    AnalogButton<uint8_t> touch2(&glove_data.touch_sensor.b);
+    touch_sensors[TOUCH2] = &touch2;
 
-  /* TOUCH3 */
-  AnalogButton<uint8_t> touch3(&glove_data.touch_sensor.c);
-  touch_sensors[TOUCH3] = &touch3;
+    /* TOUCH3 */
+    AnalogButton<uint8_t> touch3(&glove_data.touch_sensor.c);
+    touch_sensors[TOUCH3] = &touch3;
 
-  /* TOUCH4 */
-  AnalogButton<uint8_t> touch4(&glove_data.touch_sensor.d);
-  touch_sensors[TOUCH4] = &touch4;
+    /* TOUCH4 */
+    AnalogButton<uint8_t> touch4(&glove_data.touch_sensor.d);
+    touch_sensors[TOUCH4] = &touch4;
 
-  /* PITCHUP */
-  AnalogButton<float> pitchup(&glove_data.imu.orient_pitch, 0, 15, 20, true, true);
-  imu_axis[PITCHUP] = &pitchup;
+    /* PITCHUP */
+    AnalogButton<float> pitchup(&glove_data.imu.orient_pitch, 0, 15, 20, true,
+                                true);
+    imu_axis[PITCHUP] = &pitchup;
 
-  /* PITCHDOWN */
-  AnalogButton<float> pitchdown(&glove_data.imu.orient_pitch, -15, 0, 20, true, false);
-  imu_axis[PITCHDOWN] = &pitchdown;
+    /* PITCHDOWN */
+    AnalogButton<float> pitchdown(&glove_data.imu.orient_pitch, -15, 0, 20,
+                                  true, false);
+    imu_axis[PITCHDOWN] = &pitchdown;
 
-  /* ROLLLEFT */
-  AnalogButton<float> rollleft(&glove_data.imu.orient_roll, 0, 20, 20, true, true);
-  imu_axis[ROLLLEFT] = &rollleft;
+    /* ROLLLEFT */
+    AnalogButton<float> rollleft(&glove_data.imu.orient_roll, 0, 20, 20, true,
+                                 true);
+    imu_axis[ROLLLEFT] = &rollleft;
 
-  /* ROLLRIGHT */
-  AnalogButton<float> rollright(&glove_data.imu.orient_roll, -20, 0, 20, true, false);
-  imu_axis[ROLLRIGHT] = &rollright;
+    /* ROLLRIGHT */
+    AnalogButton<float> rollright(&glove_data.imu.orient_roll, -20, 0, 20, true,
+                                  false);
+    imu_axis[ROLLRIGHT] = &rollright;
 
-  /* YAWLEFT */
-  AnalogButton<float> yawleft(&glove_data.imu.orient_yaw, 0, 20, 20, true, true);
-  imu_axis[YAWLEFT] = &yawleft;
+    /* YAWLEFT */
+    AnalogButton<float> yawleft(&glove_data.imu.orient_yaw, 0, 20, 20, true,
+                                true);
+    imu_axis[YAWLEFT] = &yawleft;
 
-  /* YAWRIGHT */
-  AnalogButton<float> yawright(&glove_data.imu.orient_yaw, -20, 0, 0, true, false);
-  imu_axis[YAWRIGHT] = &yawright;
+    /* YAWRIGHT */
+    AnalogButton<float> yawright(&glove_data.imu.orient_yaw, -20, 0, 0, true,
+                                 false);
+    imu_axis[YAWRIGHT] = &yawright;
 
-  /* BUTTON MAPPING */
-  //flex_sensors[FLEX1]->change_hid_profile(KEYBOARD, 'a');
-  //AnalogButton[FLEX2].change_hid_profile();
-  //AnalogButton[FLEX3].change_hid_profile();
-  //AnalogButton[FLEX4].change_hid_profile();
-  //AnalogButton[TOUCH1].change_hid_profile();
-  //AnalogButton[TOUCH2].change_hid_profile();
-  //AnalogButton[TOUCH3].change_hid_profile();
-  //AnalogButton[TOUCH4].change_hid_profile();
-  //AnalogButton[PITCHUP].change_hid_profile();
-  //AnalogButton[PITCHDOWN].change_hid_profile();
-  //AnalogButton[ROLLLEFT].change_hid_profile();
-  //AnalogButton[ROLLRIGHT].change_hid_profile();
-  //AnalogButton[YAWLEFT].change_hid_profile();
-  //AnalogButton[YAWRIGHT].change_hid_profile();
+    /* BUTTON MAPPING */
+    // flex_sensors[FLEX1]->change_hid_profile(KEYBOARD, 'a');
+    // AnalogButton[FLEX2].change_hid_profile();
+    // AnalogButton[FLEX3].change_hid_profile();
+    // AnalogButton[FLEX4].change_hid_profile();
+    // AnalogButton[TOUCH1].change_hid_profile();
+    // AnalogButton[TOUCH2].change_hid_profile();
+    // AnalogButton[TOUCH3].change_hid_profile();
+    // AnalogButton[TOUCH4].change_hid_profile();
+    // AnalogButton[PITCHUP].change_hid_profile();
+    // AnalogButton[PITCHDOWN].change_hid_profile();
+    // AnalogButton[ROLLLEFT].change_hid_profile();
+    // AnalogButton[ROLLRIGHT].change_hid_profile();
+    // AnalogButton[YAWLEFT].change_hid_profile();
+    // AnalogButton[YAWRIGHT].change_hid_profile();
 
-  update_task_timer = new RtosTimer(this, &Translator::gestureCheck, osTimerPeriodic);
+    update_task_timer =
+          new RtosTimer(this, &Translator::gestureCheck, osTimerPeriodic);
 }
 
-//TODO: Determine FINAL process w/ transciever
-//void Translator::updateGestureMap(std::vector<AnalogButton>* updatedMapping) {
-   //sensors = *updatedMapping;
+// TODO: Determine FINAL process w/ transciever
+// void Translator::updateGestureMap(std::vector<AnalogButton>* updatedMapping)
+// {
+// sensors = *updatedMapping;
 //}
 
 void Translator::gestureCheck() {
 
-  /* Update Sensor Data */
-  flex->updateAndWriteSensors(&glove_data.flex_sensors);
-  touch->writeKeys(&glove_data.touch_sensor);
-  imu->updateAndWriteSensors(&glove_data.imu);
+    /* Update Sensor Data */
+    flex->updateAndWriteSensors(&glove_data.flex_sensors);
+    touch->writeKeys(&glove_data.touch_sensor);
+    imu->updateAndWriteSensors(&glove_data.imu);
 
-  /* Flex Sensor functionality */
-  for (int i = 0; i < FLEX_COUNT; ++i) {
+    /* Flex Sensor functionality */
+    for (int i = 0; i < FLEX_COUNT; ++i) {
 
-    /* Keyboard functionality */
-    if (flex_sensors[i]->is_keyboard()) {
-      keyboardData keyboard = flex_sensors[i]->get_keyboard_data();
-      if (keyboard.changed && keyboard.valid) {
-        if (keyboard.value) {
-          HIDinput->keyPress(keyboard.key);
-        }
-        else {
-          HIDinput->keyRelease(keyboard.key);
-        }
-      }
-    }//keyboard
+        /* Keyboard functionality */
+        if (flex_sensors[i]->is_keyboard()) {
+            keyboardData keyboard = flex_sensors[i]->get_keyboard_data();
+            if (keyboard.changed && keyboard.valid) {
+                if (keyboard.value) {
+                    HIDinput->keyPress(keyboard.key);
+                } else {
+                    HIDinput->keyRelease(keyboard.key);
+                }
+            }
+        }  // keyboard
 
-    /* Mouse functionality */
-    else if (flex_sensors[i]->is_mouse()) {
-      mouseData mouse = flex_sensors[i]->get_mouse_data(); //Grab mouse data
-      if (mouse.changed && mouse.valid) {
+        /* Mouse functionality */
+        else if (flex_sensors[i]->is_mouse()) {
+            mouseData mouse =
+                  flex_sensors[i]->get_mouse_data();  // Grab mouse data
+            if (mouse.changed && mouse.valid) {
 
-        /* Left click */
-        if (mouse.part == LBUTTON) {
-          if (mouse.value) {
-            HIDinput->setMouseButton(LEFT, DOWN);
-          }
-          else {
-            HIDinput->setMouseButton(LEFT, UP);
-          }
-        }
+                /* Left click */
+                if (mouse.part == LBUTTON) {
+                    if (mouse.value) {
+                        HIDinput->setMouseButton(LEFT, DOWN);
+                    } else {
+                        HIDinput->setMouseButton(LEFT, UP);
+                    }
+                }
 
-        /* Right click */
-        else if (mouse.part == RBUTTON) {
-          if (mouse.value) {
-            HIDinput->setMouseButton(RIGHT, DOWN);
-          }
-          else {
-            HIDinput->setMouseButton(RIGHT, UP);
-          }
-        }
+                /* Right click */
+                else if (mouse.part == RBUTTON) {
+                    if (mouse.value) {
+                        HIDinput->setMouseButton(RIGHT, DOWN);
+                    } else {
+                        HIDinput->setMouseButton(RIGHT, UP);
+                    }
+                }
 
-        /* Scroll functionality */
-        else if (mouse.part == SCROLLAXIS) {
-          HIDinput->setMouseScroll(mouse.value);
-        }
+                /* Scroll functionality */
+                else if (mouse.part == SCROLLAXIS) {
+                    HIDinput->setMouseScroll(mouse.value);
+                }
 
-        /* X-axis functionality */
-        else if (mouse.part == XAXIS) {
-          HIDinput->setMouseSpeedX(mouse.value);
-        }
+                /* X-axis functionality */
+                else if (mouse.part == XAXIS) {
+                    HIDinput->setMouseSpeedX(mouse.value);
+                }
 
-        /* Y-axis functionality */
-        else if (mouse.part == YAXIS) {
-          HIDinput->setMouseSpeedY(mouse.value);
-        }
-      }//for
-    }//mouse
-  }//for
+                /* Y-axis functionality */
+                else if (mouse.part == YAXIS) {
+                    HIDinput->setMouseSpeedY(mouse.value);
+                }
+            }  // for
+        }  // mouse
+    }  // for
 
-  /* Touch Sensor functionality */
-  for (int i = 0; i < TOUCH_COUNT; ++i) {
+    /* Touch Sensor functionality */
+    for (int i = 0; i < TOUCH_COUNT; ++i) {
 
-    /* Keyboard functionality */
-    if (touch_sensors[i]->is_keyboard()) {
-      keyboardData keyboard = touch_sensors[i]->get_keyboard_data();
-      if (keyboard.changed && keyboard.valid) {
-        if (keyboard.value) {
-          HIDinput->keyPress(keyboard.key);
-        }
-        else {
-          HIDinput->keyRelease(keyboard.key);
-        }
-      }
-    }//keyboard
+        /* Keyboard functionality */
+        if (touch_sensors[i]->is_keyboard()) {
+            keyboardData keyboard = touch_sensors[i]->get_keyboard_data();
+            if (keyboard.changed && keyboard.valid) {
+                if (keyboard.value) {
+                    HIDinput->keyPress(keyboard.key);
+                } else {
+                    HIDinput->keyRelease(keyboard.key);
+                }
+            }
+        }  // keyboard
 
-    /* Mouse functionality */
-    else if (touch_sensors[i]->is_mouse()) {
-      mouseData mouse = touch_sensors[i]->get_mouse_data(); //Grab mouse data
-      if (mouse.changed && mouse.valid) {
+        /* Mouse functionality */
+        else if (touch_sensors[i]->is_mouse()) {
+            mouseData mouse =
+                  touch_sensors[i]->get_mouse_data();  // Grab mouse data
+            if (mouse.changed && mouse.valid) {
 
-        /* Left click */
-        if (mouse.part == LBUTTON) {
-          if (mouse.value) {
-            HIDinput->setMouseButton(LEFT, DOWN);
-          }
-          else {
-            HIDinput->setMouseButton(LEFT, UP);
-          }
-        }
+                /* Left click */
+                if (mouse.part == LBUTTON) {
+                    if (mouse.value) {
+                        HIDinput->setMouseButton(LEFT, DOWN);
+                    } else {
+                        HIDinput->setMouseButton(LEFT, UP);
+                    }
+                }
 
-        /* Right click */
-        else if (mouse.part == RBUTTON) {
-          if (mouse.value) {
-            HIDinput->setMouseButton(RIGHT, DOWN);
-          }
-          else {
-            HIDinput->setMouseButton(RIGHT, UP);
-          }
-        }
+                /* Right click */
+                else if (mouse.part == RBUTTON) {
+                    if (mouse.value) {
+                        HIDinput->setMouseButton(RIGHT, DOWN);
+                    } else {
+                        HIDinput->setMouseButton(RIGHT, UP);
+                    }
+                }
 
-        /* Scroll functionality */
-        else if (mouse.part == SCROLLAXIS) {
-          HIDinput->setMouseScroll(mouse.value);
-        }
+                /* Scroll functionality */
+                else if (mouse.part == SCROLLAXIS) {
+                    HIDinput->setMouseScroll(mouse.value);
+                }
 
-        /* X-axis functionality */
-        else if (mouse.part == XAXIS) {
-          HIDinput->setMouseSpeedX(mouse.value);
-        }
+                /* X-axis functionality */
+                else if (mouse.part == XAXIS) {
+                    HIDinput->setMouseSpeedX(mouse.value);
+                }
 
-        /* Y-axis functionality */
-        else if (mouse.part == YAXIS) {
-          HIDinput->setMouseSpeedY(mouse.value);
-        }
-      }//for
-    }//mouse
-  }//for
+                /* Y-axis functionality */
+                else if (mouse.part == YAXIS) {
+                    HIDinput->setMouseSpeedY(mouse.value);
+                }
+            }  // for
+        }  // mouse
+    }  // for
 
-  /* IMU functionality */
-  for (int i = 0; i < IMU_COUNT; ++i) {
+    /* IMU functionality */
+    for (int i = 0; i < IMU_COUNT; ++i) {
 
-    /* Keyboard functionality */
-    if (imu_axis[i]->is_keyboard()) {
-      keyboardData keyboard = imu_axis[i]->get_keyboard_data();
-      if (keyboard.changed && keyboard.valid) {
-        if (keyboard.value) {
-          HIDinput->keyPress(keyboard.key);
-        }
-        else {
-          HIDinput->keyRelease(keyboard.key);
-        }
-      }
-    }//keyboard
+        /* Keyboard functionality */
+        if (imu_axis[i]->is_keyboard()) {
+            keyboardData keyboard = imu_axis[i]->get_keyboard_data();
+            if (keyboard.changed && keyboard.valid) {
+                if (keyboard.value) {
+                    HIDinput->keyPress(keyboard.key);
+                } else {
+                    HIDinput->keyRelease(keyboard.key);
+                }
+            }
+        }  // keyboard
 
-    /* Mouse functionality */
-    else if (imu_axis[i]->is_mouse()) {
-      mouseData mouse = imu_axis[i]->get_mouse_data(); //Grab mouse data
-      if (mouse.changed && mouse.valid) {
+        /* Mouse functionality */
+        else if (imu_axis[i]->is_mouse()) {
+            mouseData mouse = imu_axis[i]->get_mouse_data();  // Grab mouse data
+            if (mouse.changed && mouse.valid) {
 
-        /* Left click */
-        if (mouse.part == LBUTTON) {
-          if (mouse.value) {
-            HIDinput->setMouseButton(LEFT, DOWN);
-          }
-          else {
-            HIDinput->setMouseButton(LEFT, UP);
-          }
-        }
+                /* Left click */
+                if (mouse.part == LBUTTON) {
+                    if (mouse.value) {
+                        HIDinput->setMouseButton(LEFT, DOWN);
+                    } else {
+                        HIDinput->setMouseButton(LEFT, UP);
+                    }
+                }
 
-        /* Right click */
-        else if (mouse.part == RBUTTON) {
-          if (mouse.value) {
-            HIDinput->setMouseButton(RIGHT, DOWN);
-          }
-          else {
-            HIDinput->setMouseButton(RIGHT, UP);
-          }
-        }
+                /* Right click */
+                else if (mouse.part == RBUTTON) {
+                    if (mouse.value) {
+                        HIDinput->setMouseButton(RIGHT, DOWN);
+                    } else {
+                        HIDinput->setMouseButton(RIGHT, UP);
+                    }
+                }
 
-        /* Scroll functionality */
-        else if (mouse.part == SCROLLAXIS) {
-          HIDinput->setMouseScroll(mouse.value);
-        }
+                /* Scroll functionality */
+                else if (mouse.part == SCROLLAXIS) {
+                    HIDinput->setMouseScroll(mouse.value);
+                }
 
-        /* X-axis functionality */
-        else if (mouse.part == XAXIS) {
-          HIDinput->setMouseSpeedX(mouse.value);
-        }
+                /* X-axis functionality */
+                else if (mouse.part == XAXIS) {
+                    HIDinput->setMouseSpeedX(mouse.value);
+                }
 
-        /* Y-axis functionality */
-        else if (mouse.part == YAXIS) {
-          HIDinput->setMouseSpeedY(mouse.value);
-        }
-      }//for
-    }//mouse
-  }//for
+                /* Y-axis functionality */
+                else if (mouse.part == YAXIS) {
+                    HIDinput->setMouseSpeedY(mouse.value);
+                }
+            }  // for
+        }  // mouse
+    }  // for
 
-  /* Send HID inputs */
-  HIDinput->sendKeyboard();
-  HIDinput->sendMouse();
+    /* Send HID inputs */
+    HIDinput->sendKeyboard();
+    HIDinput->sendMouse();
 
-  return;
+    return;
 }
 
-void Translator::startUpdateTask(uint32_t ms) {
-  update_task_timer->start(ms);
-}
-void Translator::stopUpdateTask() {
-  update_task_timer->stop();
-}
+void Translator::startUpdateTask(uint32_t ms) { update_task_timer->start(ms); }
+void Translator::stopUpdateTask() { update_task_timer->stop(); }
--- mbed-glove-firmware/drivers/translator.h
+++ mbed-glove-firmware/drivers/translator.h
@@ -33,27 +33,27 @@
 const uint32_t COLLECTOR_UPDATE_PERIOD = 10;
 
 enum FLEX {
-                     FLEX1,
-                     FLEX2,
-                     FLEX3,
-                     FLEX4
-                   };
+    FLEX1,
+    FLEX2,
+    FLEX3,
+    FLEX4
+};
 
 enum TOUCH {
-                     TOUCH1,
-                     TOUCH2,
-                     TOUCH3,
-                     TOUCH4
-                   };
+    TOUCH1,
+    TOUCH2,
+    TOUCH3,
+    TOUCH4
+};
 
 enum IMU {
-                     PITCHUP,
-                     PITCHDOWN,
-                     ROLLLEFT,
-                     ROLLRIGHT,
-                     YAWLEFT,
-                     YAWRIGHT
-                   };
+    PITCHUP,
+    PITCHDOWN,
+    ROLLLEFT,
+    ROLLRIGHT,
+    YAWLEFT,
+    YAWRIGHT
+};
 
 /* Translator
  *
@@ -64,52 +64,53 @@ enum IMU {
  */
 class Translator {
 public:
-  /*
-   * Constructor for translator
-   *
-   */
-  Translator(FlexSensors* flex, IMU_BNO055* imu, TouchSensor* touch, KeyboardMouse* input);
+    /*
+     * Constructor for translator
+     *
+     */
+    Translator(FlexSensors* flex, IMU_BNO055* imu, TouchSensor* touch,
+               KeyboardMouse* input);
 
-  /*
-   * Update gesture mapping via new configuration vector.
-   * Transciever to send the new Vector to bluetooth class,
-   * which should then call this function
-   */
-  //void updateGestureMap(std::vector<AnalogButton>* updatedMapping);
+    /*
+     * Update gesture mapping via new configuration vector.
+     * Transciever to send the new Vector to bluetooth class,
+     * which should then call this function
+     */
+    // void updateGestureMap(std::vector<AnalogButton>* updatedMapping);
 
-  /*
-   * Analyze sensors to determine if gesture
-   * is occuring. If so, generate proper HID data to be sent to HID class.
-   * This function designed to be set up as a periodic task.
-   */
-  void gestureCheck();
+    /*
+     * Analyze sensors to determine if gesture
+     * is occuring. If so, generate proper HID data to be sent to HID class.
+     * This function designed to be set up as a periodic task.
+     */
+    void gestureCheck();
 
-  /*
-   * Calls the start() method on the periodic update task,
-   * an internal timer is set up in the constructor
-   */
-  void startUpdateTask(uint32_t ms=COLLECTOR_UPDATE_PERIOD);
+    /*
+     * Calls the start() method on the periodic update task,
+     * an internal timer is set up in the constructor
+     */
+    void startUpdateTask(uint32_t ms = COLLECTOR_UPDATE_PERIOD);
 
-  /*
-   * Calls the stop() method on the periodic update timer,
-   */
-  void stopUpdateTask();
+    /*
+     * Calls the stop() method on the periodic update timer,
+     */
+    void stopUpdateTask();
 
 private:
-  //NOTE: Vector indexed by GESTURE enum
-  AnalogButton<flex_sensor_t>* flex_sensors[FLEX_COUNT];
-  AnalogButton<uint8_t>* touch_sensors[TOUCH_COUNT];
-  AnalogButton<float>* imu_axis[IMU_COUNT];
-  KeyboardMouse* HIDinput; //KeyboardMouse object
+    // NOTE: Vector indexed by GESTURE enum
+    AnalogButton<flex_sensor_t>* flex_sensors[FLEX_COUNT];
+    AnalogButton<uint8_t>* touch_sensors[TOUCH_COUNT];
+    AnalogButton<float>* imu_axis[IMU_COUNT];
+    KeyboardMouse* HIDinput;  // KeyboardMouse object
 
-  //Glove data
-  FlexSensors* flex;
-  IMU_BNO055* imu;
-  TouchSensor* touch;
-  glove_sensors_raw_t glove_data;
+    // Glove data
+    FlexSensors* flex;
+    IMU_BNO055* imu;
+    TouchSensor* touch;
+    glove_sensors_raw_t glove_data;
 
-  RtosTimer* update_task_timer;
-  DigitalOut working;
+    RtosTimer* update_task_timer;
+    DigitalOut working;
 };
 
 #endif /* TRANSLATOR_H_ */
