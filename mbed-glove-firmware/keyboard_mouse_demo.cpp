f984b385b157d957b72d7cfaec1376ab11c51545
Adrian Padin <apadin1@users.noreply.github.com>
2016-11-15 13:57:40 -0500

    Update keyboard_mouse_demo.cpp

--- mbed-glove-firmware/keyboard_mouse_demo.cpp
+++ mbed-glove-firmware/keyboard_mouse_demo.cpp
@@ -15,39 +15,33 @@
  */
 
 #include "mbed.h"
-#include "drivers/keyboard_mouse.h"
+#include "keyboard_mouse.h"
+#include "flex_sensor.h"
+#include "touch_sensor.h"
 
 #define LED_OFF 1
 #define LED_ON 0
-
-const char* ble_errors[] = {
-    "BLE_ERROR_NONE",
-    "BLE_ERROR_BUFFER_OVERFLOW",
-    "BLE_ERROR_NOT_IMPLEMENTED",
-    "BLE_ERROR_PARAM_OUT_OF_RANGE",
-    "BLE_ERROR_INVALID_PARAM",
-    "BLE_STACK_BUSY",
-    "BLE_ERROR_INVALID_STATE",
-    "BLE_ERROR_NO_MEM",
-    "BLE_ERROR_OPERATION_NOT_PERMITTED",
-    "BLE_ERROR_INITIALIZATION_INCOMPLETE",
-    "BLE_ERROR_ALREADY_INITIALIZED",
-    "BLE_ERROR_UNSPECIFIED",
-    "BLE_ERROR_INTERNAL_STACK_FAILURE"
-};
 
 /* LEDs and Buttons */
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
-DigitalOut db(p12);
+
+DigitalOut pin15(P0_15);
+DigitalOut pin16(P0_16);
 
 InterruptIn button1(BUTTON1);
 InterruptIn button2(BUTTON2);
 InterruptIn button3(BUTTON3);
 InterruptIn button4(BUTTON4);
 
+FlexSensors flex_sensors_class;
+flex_sensor_t flex_sensor_array[4];
+
+TouchSensor touch_sensor_class;
+key_states_t keys;
+key_states_t last_keys;
 
 KeyboardMouse * keyboard_ptr;
 
@@ -56,102 +50,96 @@ static void waiting() {
     if (!keyboard_ptr->isConnected())
         led1 = !led1; //blink led1
     else
+        return;
         led2 = !led2; //blink led2
 }
 
 // Button Callbacks
-void button1pressed() {
-    db = 1;
-    keyboard_ptr->keyPress('a', 0);
-    db = 0;
-}
-void button1released() {
-    db = 1;
-    keyboard_ptr->keyRelease();
-    db = 0;
-}
+void button1pressed() { keyboard_ptr->keyPress('b'); }
+void button1released() { keyboard_ptr->keyRelease('b'); }
 
-void button2pressed() {
-    db = 1;
-    keyboard_ptr->keyPress('b', 0);
-    db = 0;
-}
-void button2released() {
-    db = 1;
-    keyboard_ptr->keyRelease();
-    db = 0;
-}
+void button2pressed() { keyboard_ptr->keyPress(UP_ARROW); }
+void button2released() { keyboard_ptr->keyRelease(UP_ARROW); }
 
-void button3pressed() {
-    db = 1;
-    keyboard_ptr->setMouseSpeedAll(10, 0, 0);
-    db = 0;
-}
+void button3pressed() { keyboard_ptr->setMouseSpeedAll(10, 0, 0); }
 void button3released() { keyboard_ptr->setMouseSpeedAll(0, 0, 0); }
 
-void button4pressed() {
-    db = 1;
-    keyboard_ptr->setMouseSpeedAll(-10, 0, 0);
-    db = 0;
-}
-void button4released() {
-    db = 1;
-    keyboard_ptr->setMouseSpeedAll(0, 0, 0);
-    db = 0;
-}
+void button4pressed() { keyboard_ptr->setMouseSpeedAll(-10, 0, 0); }
+void button4released() { keyboard_ptr->setMouseSpeedAll(0, 0, 0); }
 
+void translate() {
+    pin15 = 1;
 
-ble_error_t err;
-Serial dbg(USBTX, USBRX);
+    /* Flex Sensors */
+    flex_sensors_class.update();
+    flex_sensors_class.writeSensors(flex_sensor_array);
+    
+    if (flex_sensor_array[0] < 250) { keyboard_ptr->keyPress(' '); }
+    else { keyboard_ptr->keyRelease(' '); }
+    
+    touch_sensor_class.update();
+    touch_sensor_class.writeKeys(&keys);
 
-void auto_button() {
-    db = 1;
-    err = keyboard_ptr->keyPress('x', 0);
-    db = 0;
-    dbg.printf("%s\r\n", ble_errors[err]);
+    /* Touch Sensors */
 
-    wait_ms(500);
+    //if (last_keys.a != keys.a) {
+    //    if (keys.a) keyboard_ptr->keyPress('d');
+    //    else        keyboard_ptr->keyRelease('d');
+    //}
+    
+    if (last_keys.b != keys.b) {
+        if (keys.b) keyboard_ptr->keyPress('d');
+        else        keyboard_ptr->keyRelease('d');
+    }
+    
+    if (last_keys.c != keys.c) {
+        if (keys.c) keyboard_ptr->keyPress('s');
+        else        keyboard_ptr->keyRelease('s');
+    }
 
-    db = 1;
-    err = keyboard_ptr->keyRelease();
-    db = 0;
-    dbg.printf("%s\r\n", ble_errors[err]);
+    if (last_keys.d != keys.d) {
+        if (keys.d) keyboard_ptr->keyPress('a');
+        else        keyboard_ptr->keyRelease('a');
+    }
+    
+    last_keys = keys;
+    pin15 = 0;
+    
+    keyboard_ptr->sendKeyboard();
+    keyboard_ptr->sendMouse();
 }
 
 
 // MAIN
-int keyboard_mouse_demo() {
+int main() {
 
-    wait(5);
-
-    db = 0;
-    //printf("start here\r\n");
     KeyboardMouse kbdMouse;
     keyboard_ptr = &kbdMouse;
+    
+    //Ticker waiting_tick;
+    //waiting_tick.attach(waiting, 1);
 
-    //printf("init ticker\r\n");
-    Ticker waiting_tick;
-    waiting_tick.attach(waiting, 1);
+    Callback<void()> wait_callback(waiting);
+    RtosTimer wait_timer(wait_callback, osTimerPeriodic);
+    wait_timer.start(100);
 
-    //printf("init buttons\r\n");
     led1 = LED_OFF;
     led2 = LED_OFF;
     led3 = LED_OFF;
     led4 = LED_OFF;
 
-    Ticker keyboard_test;
-    keyboard_test.attach(auto_button, 3);
-
-    /*
     button1.fall(button1pressed);
     button1.rise(button1released);
-    button2.fall(button2pressed);
-    button2.rise(button2released);
-    button3.fall(button3pressed);
-    button3.rise(button3released);
-    button4.fall(button4pressed);
-    button4.rise(button4released);
-    */
+    //button2.fall(button2pressed);
+    //button2.rise(button2released);
+    //button3.fall(button3pressed);
+    //button3.rise(button3released);
+    //button4.fall(button4pressed);
+    //button4.rise(button4released);
+
+    Callback<void()> translate_callback(translate);
+    RtosTimer translate_timer(translate_callback, osTimerPeriodic);
+    translate_timer.start(10);
 
     while (true) {
         keyboard_ptr->waitForEvent();
