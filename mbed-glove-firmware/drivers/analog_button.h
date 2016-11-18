492bf3822e716f83bf21a108cd84ea0773fa4cad
Ryan OKeefe <rpokeefe@umich.edu>
2016-11-18 11:03:00 -0500

    almost compiles v3

--- mbed-glove-firmware/drivers/analog_button.h
+++ mbed-glove-firmware/drivers/analog_button.h
@@ -19,186 +19,13 @@
 #ifndef ANALOG_BUTTON_H_
 #define ANALOG_BUTTON_H_
 #include <stdint.h>
-enum hidType {
-    KEYBOARD,
-    MOUSE,
-    JOYSTICK,
-    DISABLED
-};
-
-enum mousePart {
-    LBUTTON,
-    RBUTTON,
-    SCROLLAXIS,
-    XAXIS,
-    YAXIS,
-    NONE
-};
-
-struct keyboardData {
-    bool valid;
-    bool value;
-    char key;
-    bool operator==(const keyboardData& a) { return (value == a.value); }
-};
-
-struct mouseData {
-    bool valid;
-    mousePart part;
-    int8_t value;
-    bool operator==(const mouseData& a) { return (value == a.value); }
-};
-
-class flexToHID {
-    public:
-    flexToHID( uint16_t* data_, uint16_t min_, uint16_t max_, float transition_band, bool active_low_=false)
-        : active_low(active_low_) {
-        sensor_conversion = AnalogButton<uint16_t>(data_, min_, max_, transition_band_, active_low_);
-        cur_keyboard = keyboardData();
-        cur_mouse = mouseData();
-    }
-    
-    keyboardData get_keyboard_data() {
-        cur_keyboard.value = sensor_conversion.get_binary_state();
-        return cur_keyboard;
-    }
-
-    mouseData get_mouse_data() {
-        cur_mouse.valid = true;
-        if (cur_mouse.part == LBUTTON || cur_mouse.part == RBUTTON)
-            cur_mouse.value = sensor_conversion.get_binary_state();
-        else {
-            cur_mouse.valid = false;
-        }
-        return cur_mouse;
-    }
-    
-    bool is_keyboard() { return HID == KEYBOARD; }
-    bool is_mouse() { return HID == MOUSE; }
-    bool is_joystick() { return HID == JOYSTICK; }
-
-    // call this function to change the hid status of the
-    void change_hid_profile(hidType hid, char key_=0,
-                            mousePart part_ = NONE) {
-        HID = hid;
-        if (HID == KEYBOARD) {
-            cur_keyboard.valid = true;
-            cur_keyboard.key = key_;
-        } else if (HID == MOUSE) {
-            cur_mouse.valid = true;
-            cur_mouse.part = part_;
-        } 
-    }
-
-    private:
-    bool active_low;
-    AnalogButton <uint16_t> sensor_conversion;
-    hidType HID;
-    keyboardData cur_keyboard;
-    mouseData cur_mouse;
-
-};
-
-class imuToHID {
-    public:
-    imuToHID( float* data_, float min_, float max_, float transition_band, bool active_low_=false)
-        : active_low(active_low_) {
-        sensor_conversion = AnalogButton<float>(data_, min_, max_, transition_band_, active_low_);
-        cur_keyboard = keyboardData();
-        cur_mouse = mouseData();
-    }
-    
-    keyboardData get_keyboard_data() {
-        cur_keyboard.value = sensor_conversion.get_binary_state();
-        return cur_keyboard;
-    }
-
-    mouseData get_mouse_data() {
-        cur_mouse.valid = true;
-        if (cur_mouse.part == LBUTTON || cur_mouse.part == RBUTTON)
-            cur_mouse.value = sensor_conversion.get_binary_state();
-        else {
-            cur_mouse.valid = false;
-        }
-        return cur_mouse;
-    }
-    
-    bool is_keyboard() { return HID == KEYBOARD; }
-    bool is_mouse() { return HID == MOUSE; }
-    bool is_joystick() { return HID == JOYSTICK; }
-
-    // call this function to change the hid status of the
-    void change_hid_profile(hidType hid, char key_=0,
-                            mousePart part_ = NONE) {
-        HID = hid;
-        if (HID == KEYBOARD) {
-            cur_keyboard.valid = true;
-            cur_keyboard.key = key_;
-        } else if (HID == MOUSE) {
-            cur_mouse.valid = true;
-            cur_mouse.part = part_;
-        } 
-    }
-
-    private:
-    bool active_low;
-    AnalogButton <uint16_t> sensor_conversion;
-    hidType HID;
-    keyboardData cur_keyboard;
-    mouseData cur_mouse;
-};
-
-class touchToHID {
-public:
-  touchToHID(key_states_t* data_)
-      : data(data_) {
-      cur_keyboard = keyboardData();
-      cur_mouse = mouseData(); //Only for mouse clicks
-  }
-
-  // these are functions to check the HID status of the sensor
-  bool is_keyboard() { return HID == KEYBOARD; }
-  bool is_mouse() { return HID == MOUSE; }
-  bool is_joystick() { return HID == JOYSTICK; }
-
-  // call this function to change the hid status of the
-  void change_hid_profile(hidType hid, char key_=0,
-                          mousePart part_ = NONE) {
-      HID = hid;
-      if (HID == KEYBOARD) {
-         cur_keyboard.valid = true;
-         cur_keyboard.key = key_;
-      } else if (HID == MOUSE) {
-         cur_mouse.valid = true;
-         cur_mouse.part = part_;
-      } else {
-         cur_keyboard.valid = false;
-         cur_mouse.valid = false;
-      }
-  }
-
-  keyboardData get_keyboard_data() {
-    cur_keyboard.value = *data;
-    return cur_keyboard;
-  }
-
-  mouseData get_mouse_data() {
-    cur_mouse.value = *data;
-    return cur_mouse;
-  }
-
-private:
-  key_states_t* data;
-
-  hidType HID;
-  keyboardData cur_keyboard;
-  mouseData cur_mouse;
-};
 
 template <class T>
 class AnalogButton {
 public:
-
+    AnalogButton() {
+        //TODO:fix this shit
+    }
     AnalogButton(T* data_, T min_, T max_, float transition_band, bool active_low_)
         : data(data_), min_abs(min_), max_abs(max_), active_low(active_low_) {
 
@@ -251,15 +78,15 @@ public:
     uint8_t analog_read() {
         T value = (*data < max_abs) ? *data : max_abs;
         value = (*data > min_abs) ? *data : min_abs;
-        if (imu) {
-            int8_t temp = 127 * (value / range);
-        if (temp > 10 || temp < -10)
-            cur_mouse.value = temp;
-        else
-            cur_mouse.value = 0;
-        } else {
-            int8_t temp = 127 * (value / range);
-        
+        int8_t temp = 0;
+        if (true) {
+            temp = 127 * (value / range);
+            if (temp > 10 || temp < -10)
+                temp = temp;//AAAAGGGHHGHHGHH
+            else
+                temp = 0;
+        }
+        return temp;
     }
     T* data;
     T range;
@@ -268,37 +95,62 @@ private:
     bool active_low;
     T min_abs, max_abs;
     T min_thresh, max_thresh;
+};
 
-#endif /*ANALOG_BUTTON_H_*/
+enum hidType {
+    KEYBOARD,
+    MOUSE,
+    JOYSTICK,
+    DISABLED
+};
 
+enum mousePart {
+    LBUTTON,
+    RBUTTON,
+    SCROLLAXIS,
+    XAXIS,
+    YAXIS,
+    NONE
+};
 
+struct keyboardData {
+    bool valid;
+    bool value;
+    char key;
+    bool operator==(const keyboardData& a) { return (value == a.value); }
+};
 
+struct mouseData {
+    bool valid;
+    mousePart part;
+    int8_t value;
+    bool operator==(const mouseData& a) { return (value == a.value); }
+};
 
-
-
-template <class T> class OLDAnalogButton {
-public:
-    // Use this cTor for analog input that may be mapped to analog or digital
-    // HID output
-    AnalogButton() {}
-    AnalogButton(T* data_, T min_, T max_, float transition_band,
-                 bool imu_=true, bool positive_range=true,
-                 bool active_low_=false)
-        : data(data_), min_abs(min_), max_abs(max_), active_low(active_low_),
-          is_analog(true), imu(imu_), pos_range(positive_range) {
-
-        update_threshold(transition_band);
+class flexToHID {
+    public:
+    flexToHID( uint16_t* data_, uint16_t min_, uint16_t max_, float transition_band_, bool active_low_=false)
+        : active_low(active_low_) {
+        sensor_conversion = AnalogButton<uint16_t>(data_, min_, max_, transition_band_, active_low_);
         cur_keyboard = keyboardData();
         cur_mouse = mouseData();
     }
-
-    // Use this cTor for pure digital buttons
-    AnalogButton(T* data_, bool active_low_=0)
-        : data(data_), active_low(active_low_), max_abs(0), is_analog(false) {
-        cur_keyboard = keyboardData();
+    
+    keyboardData get_keyboard_data() {
+        cur_keyboard.value = sensor_conversion.get_binary_state();
+        return cur_keyboard;
     }
 
-    // these are functions to check the HID status of the sensor
+    mouseData get_mouse_data() {
+        cur_mouse.valid = true;
+        if (cur_mouse.part == LBUTTON || cur_mouse.part == RBUTTON)
+            cur_mouse.value = sensor_conversion.get_binary_state();
+        else {
+            cur_mouse.valid = false;
+        }
+        return cur_mouse;
+    }
+    
     bool is_keyboard() { return HID == KEYBOARD; }
     bool is_mouse() { return HID == MOUSE; }
     bool is_joystick() { return HID == JOYSTICK; }
@@ -307,153 +159,118 @@ public:
     void change_hid_profile(hidType hid, char key_=0,
                             mousePart part_ = NONE) {
         HID = hid;
-        if (is_analog) {
-            if (HID == KEYBOARD) {
-                update_value = &AnalogButton<T>::analog_to_digital_read;
-                cur_keyboard.changed = true;
-                cur_keyboard.valid = true;
-                cur_keyboard.key = key_;
-            } else if (HID == MOUSE) {
-                cur_mouse.changed = true;
-                cur_mouse.valid = true;
-                cur_mouse.part = part_;
-                if (part_ == LBUTTON || part_ == RBUTTON)
-                    update_value = &AnalogButton<T>::analog_to_digital_read;
-                else
-                    update_value = &AnalogButton<T>::analog_read;
-            }
-        } else {
-            if (HID == KEYBOARD) {
-                update_value = &AnalogButton<T>::digital_read;
-                cur_keyboard.changed = true;
-                cur_keyboard.valid = true;
-                cur_keyboard.key = key_;
-            } else if (HID == MOUSE) {
-                cur_mouse.changed = true;
-                cur_mouse.valid = true;
-                cur_mouse.part = part_;
-                update_value = &AnalogButton<T>::digital_read;
-            }
-        }
-        (this->*update_value)();
+        if (HID == KEYBOARD) {
+            cur_keyboard.valid = true;
+            cur_keyboard.key = key_;
+        } else if (HID == MOUSE) {
+            cur_mouse.valid = true;
+            cur_mouse.part = part_;
+        } 
     }
+
+    private:
+    bool active_low;
+    AnalogButton<uint16_t> sensor_conversion;
+    hidType HID;
+    keyboardData cur_keyboard;
+    mouseData cur_mouse;
+
+};
+
+class imuToHID {
+    public:
+    imuToHID( float* data_, float min_, float max_, float transition_band_, bool active_low_=false)
+        : active_low(active_low_) {
+        sensor_conversion = AnalogButton<float>(data_, min_, max_, transition_band_, active_low_);
+        cur_keyboard = keyboardData();
+        cur_mouse = mouseData();
+    }
+    
     keyboardData get_keyboard_data() {
-        keyboardData prev = cur_keyboard;
-        prev.valid = false;
-        if (HID != KEYBOARD)
-            return prev;
-        (this->*update_value)();
-        if (cur_keyboard == prev)
-            cur_keyboard.changed = false;
-        else
-            cur_keyboard.changed = true;
+        cur_keyboard.value = sensor_conversion.get_binary_state();
         return cur_keyboard;
     }
 
     mouseData get_mouse_data() {
-        mouseData prev = cur_mouse;
-        prev.valid = false;
-        if (HID != MOUSE)
-            return prev;
-        (this->*update_value)();
-        if (cur_mouse == prev)
-            cur_mouse.changed = false;
-        else
-            cur_mouse.changed = true;
+        cur_mouse.valid = true;
+        if (cur_mouse.part == LBUTTON || cur_mouse.part == RBUTTON)
+            cur_mouse.value = sensor_conversion.get_binary_state();
+        else {
+            cur_mouse.valid = false;
+        }
         return cur_mouse;
     }
-    void update_bounds(T min_, T max_, float transition_band) {
-        min_abs = min_;
-        max_abs = max_;
-        update_threshold(transition_band);
+    
+    bool is_keyboard() { return HID == KEYBOARD; }
+    bool is_mouse() { return HID == MOUSE; }
+    bool is_joystick() { return HID == JOYSTICK; }
+
+    // call this function to change the hid status of the
+    void change_hid_profile(hidType hid, char key_=0,
+                            mousePart part_ = NONE) {
+        HID = hid;
+        if (HID == KEYBOARD) {
+            cur_keyboard.valid = true;
+            cur_keyboard.key = key_;
+        } else if (HID == MOUSE) {
+            cur_mouse.valid = true;
+            cur_mouse.part = part_;
+        } 
     }
 
-private:
-    void (AnalogButton::*update_value)();
-    bool is_analog;
+    private:
     bool active_low;
-    bool imu;
-    bool pos_range;
-    bool enabled;
-    T* data;
-    T min_abs, max_abs;
-    T min_thresh, max_thresh;
-    T range;
-
+    AnalogButton <float> sensor_conversion;
     hidType HID;
     keyboardData cur_keyboard;
     mouseData cur_mouse;
-    // TODO: make sensor able to be deactivated so that analog imu axis can be
-    // handled by one sensor and not both
-    void update_threshold(float transition_band) {
-        float div;
-        uint8_t mult;
-        if (imu) {
-            mult = 3;
-            div = 4.0;
-        } else {
-            mult = 1;
-            div = 2.0;
-        }
-        if (transition_band < 0.0 || 1.0 < transition_band) {
-            // fail
-            return;
-        }
-        float valid_band;
-        range = max_abs - min_abs;
-        valid_band = range * ((1 - transition_band) / div);
-        if (pos_range) {
-            min_thresh = min_abs + valid_band;
-            max_thresh = max_abs - (mult * valid_band);
-        } else {
-            min_thresh = min_abs + (mult * valid_band);
-            max_thresh = max_abs - valid_band;
-        }
-    }
-    /*
-     * This is the thing that does the threshold arithmatic
-     * Counts on the data changing spurriously
-     * Active low for now cuz sure
-     */
-    void analog_to_digital_read() {
-        // in the lowest range
-        if (*data < min_thresh) {
-            if (HID == KEYBOARD)
-                cur_keyboard.value = active_low;
-            else if (HID == MOUSE)
-                cur_mouse.value = active_low;
-        }
-        // in the transition band
-        else if (*data < max_thresh) {
-            // dont change state
-        }
-        // in the upper range
-        else {
-            if (HID == KEYBOARD)
-                cur_keyboard.value = !active_low;
-            else if (HID == MOUSE)
-                cur_mouse.value = !active_low;
-        }
-    }
-
-    void analog_read() {
-        T value = (*data < max_abs) ? *data : max_abs;
-        value = (*data > min_abs) ? *data : min_abs;
-        if (imu) {
-            int8_t temp = 127 * (value / range);
-            if (temp > 10 || temp < -10)
-                cur_mouse.value = temp;
-            else
-                cur_mouse.value = 0;
-        } else {
-            int8_t temp = 127 * (value / range);
-        }
-    }
-
-    void digital_read() {
-        if (*data > 0)
-            cur_keyboard.value = 1;
-        else
-            cur_keyboard.value = 0;
-    }
 };
+
+class touchToHID {
+public:
+  touchToHID(uint8_t* data_)
+      : data(data_) {
+      cur_keyboard = keyboardData();
+      cur_mouse = mouseData(); //Only for mouse clicks
+  }
+
+  // these are functions to check the HID status of the sensor
+  bool is_keyboard() { return HID == KEYBOARD; }
+  bool is_mouse() { return HID == MOUSE; }
+  bool is_joystick() { return HID == JOYSTICK; }
+
+  // call this function to change the hid status of the
+  void change_hid_profile(hidType hid, char key_=0,
+                          mousePart part_ = NONE) {
+      HID = hid;
+      if (HID == KEYBOARD) {
+         cur_keyboard.valid = true;
+         cur_keyboard.key = key_;
+      } else if (HID == MOUSE) {
+         cur_mouse.valid = true;
+         cur_mouse.part = part_;
+      } else {
+         cur_keyboard.valid = false;
+         cur_mouse.valid = false;
+      }
+  }
+
+  keyboardData get_keyboard_data() {
+    cur_keyboard.value = *data;
+    return cur_keyboard;
+  }
+
+  mouseData get_mouse_data() {
+    cur_mouse.value = *data;
+    return cur_mouse;
+  }
+
+private:
+  uint8_t* data;
+
+  hidType HID;
+  keyboardData cur_keyboard;
+  mouseData cur_mouse;
+};
+
+#endif /*ANALOG_BUTTON_H_*/
