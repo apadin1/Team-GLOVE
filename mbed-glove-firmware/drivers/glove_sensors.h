b360bc78988f7a90854e4cd75deda9860e938200
Tim <twschum@gmail.com>
2016-11-29 16:46:47 -0500

    New cpp for glove_sensors

--- mbed-glove-firmware/drivers/glove_sensors.h
+++ mbed-glove-firmware/drivers/glove_sensors.h
@@ -53,34 +53,8 @@ typedef struct {
     float y;
 } glove_sensors_compressed_t;
 
-void compressGloveSensors(glove_sensors_raw_t* raw, glove_sensors_compressed_t* compressed) {
-    compressed->f0 = raw->flex_sensors[0];
-    compressed->f1 = raw->flex_sensors[1];
-    compressed->f2 = raw->flex_sensors[2];
-    compressed->f3 = raw->flex_sensors[3];
-
-    compressed->t = raw->touch_sensor.pack();
-
-    compressed->r = raw->imu.orient_roll;
-    compressed->p = raw->imu.orient_pitch;
-    compressed->y = raw->imu.orient_yaw;
-}
-
-void extractGloveSensors(glove_sensors_raw_t* raw, glove_sensors_compressed_t* compressed) {
-    raw->flex_sensors[0] = compressed->f0;
-    raw->flex_sensors[1] = compressed->f1;
-    raw->flex_sensors[2] = compressed->f2;
-    raw->flex_sensors[3] = compressed->f3;
-
-    raw->touch_sensor.a = (compressed->t & 0x8) >> 3;
-    raw->touch_sensor.b = (compressed->t & 0x4) >> 2;
-    raw->touch_sensor.c = (compressed->t & 0x2) >> 1;
-    raw->touch_sensor.d = (compressed->t & 0x1);
-
-    raw->imu.orient_roll = compressed->r;
-    raw->imu.orient_pitch = compressed->p;
-    raw->imu.orient_yaw = compressed->y;
-}
+void compressGloveSensors(glove_sensors_raw_t* raw, glove_sensors_compressed_t* compressed);
+void extractGloveSensors(glove_sensors_raw_t* raw, glove_sensors_compressed_t* compressed);
 
 #endif /* GLOVE_SENSORS_H_ */
 
