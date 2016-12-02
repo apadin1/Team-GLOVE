b69060b23d0282d6aee87f9d651f38ee7a7ab877
bheckathorn13 <bheck@umich.edu>
2016-12-02 11:23:27 -0500

    start scan now runs continuously

--- mbed-dongle-firmware/drivers/scanner.h
+++ mbed-dongle-firmware/drivers/scanner.h
@@ -69,7 +69,7 @@ public:
     }
 
     // Stop and start scanning
-    void startScan(int scan_period=15, int scan_duration=10) {
+    void startScan(int scan_period=10, int scan_duration=10) {
         ble.gap().setScanParams(scan_period, scan_duration);
         ble.gap().startScan(advertisementCallback);
     }
