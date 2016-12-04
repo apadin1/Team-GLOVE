2c6cbb1185ea070e600be87f017f8a4276e71d74
Tim <twschum@gmail.com>
2016-12-04 18:09:41 -0500

    Expanded the DotStar API

--- mbed-glove-firmware/drivers/dot_star_leds.h
+++ mbed-glove-firmware/drivers/dot_star_leds.h
@@ -24,6 +24,8 @@ const PinName DOT_STAR_SCLK = p18;
 
 const uint8_t DOT_STAR_DEFAULT_BRIGHTNESS = 4; // 5-bit value 0-31
 
+enum class DotStarColor {Red, Green, Blue, Orange, Yellow, Purple, Cyan, Magenta, White, Off};
+
 class DotStarLEDs {
 public:
 
@@ -36,6 +38,23 @@ public:
      */
     DotStarLEDs(uint8_t num_leds=2, PinName mosi=DOT_STAR_MOSI,
             PinName miso=DOT_STAR_MISO, PinName sclk=DOT_STAR_SCLK);
+
+    /*
+     * Select LED in the strand (zero indexed, do your own bounds check, nerd)
+     * Sets the RGB values to make the specified color
+     * birghtness is 0-31
+     */
+    void set_color(uint8_t led, DotStarColor color,
+            uint8_t brightness=DOT_STAR_DEFAULT_BRIGHTNESS);
+
+    /*
+     * Set all LEDs in the strand
+     * Sets the RGB values to make the specified color
+     * birghtness is 0-31
+     */
+    void set_color_all(DotStarColor color,
+            uint8_t brightness=DOT_STAR_DEFAULT_BRIGHTNESS);
+
     /*
      * Select LED in the strand (zero indexed, do your own bounds check)
      * red, green, blue are 0-255
@@ -52,6 +71,7 @@ public:
     void set_RGB_all(uint8_t red, uint8_t green, uint8_t blue,
             uint8_t brightness=DOT_STAR_DEFAULT_BRIGHTNESS);
 
+
 private:
     uint8_t num_leds;
     SPI spi;
@@ -59,6 +79,8 @@ private:
     uint32_t* leds;
 
     void flush_to_spi();
+    void set_led_rgb(uint8_t led, uint8_t red, uint8_t green,
+            uint8_t blue, uint8_t brightness);
 };
 
 /*
