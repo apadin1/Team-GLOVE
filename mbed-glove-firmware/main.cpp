8ade3a09f4bd2088d90aee4b4bb3d6d86b4b6289
Tim <twschum@gmail.com>
2016-12-07 19:44:21 -0500

    Comment

--- mbed-glove-firmware/main.cpp
+++ mbed-glove-firmware/main.cpp
@@ -55,6 +55,7 @@ bool check_signal_conditions(const glove_sensors_raw_t& glove_data, DotStarLEDs&
      * Indicate the first flex sensor is pulled
      */
     if (glove_data.flex_sensors[0] < 450) {
+        //TODO match the portal colors
         //leds.set_color_all(Red, 10);
         leds.set_color_all(Cyan, 8);
         return false;
